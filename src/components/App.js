94c3dfd66b0eed3ce253ba89be356cdfc0e5be85
Joseph <wamiti8711@gmail.com>
2023-01-23 14:34:57 -0500

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
@@ -6,6 +6,8 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
   const [questions, setQuestions] = useState([]);
+
+  // Add useEffect hook
   useEffect(() => {
     fetch("http://localhost:4000/questions")
       .then((r) => r.json())
@@ -13,12 +15,13 @@ function App() {
   }, []);
   function onAddQuestion(newQuestion) {
     setQuestions([ ...questions,newQuestion]);
-    console.log("new questions :", newQuestion);
+    
   }
+  //delete
   function onDelete(deletedQuestion){
     const updatedItems = questions.filter((question) => question.id !== deletedQuestion.id);
     setQuestions(updatedItems);
-    console.log("deleted questions :", deletedQuestion);
+    
   }
   function onUpdateQuestion(updatedQuestion){
     const updatedQuestions = questions.map((q) => {
@@ -27,21 +30,19 @@ function App() {
     });
     
     setQuestions(updatedQuestions);
-    console.log(updatedQuestions);
-    console.log("update questions :", updatedQuestions);
+    
   }
-
-
   return (
     <main>
-    <AdminNavBar onChangePage={setPage} />
-    {page === "Form" ? (
-      <QuestionForm onAddQuestion={onAddQuestion} />
-    ) : (
-      <QuestionList questions={questions} onDelete={onDelete} updateQuestion={onUpdateQuestion} />
-    )}
-  </main>
+      <AdminNavBar onChangePage={setPage} />
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={onAddQuestion} />
+      ) : (
+        <QuestionList questions={questions} onDelete={onDelete} updateQuestion={onUpdateQuestion} />
+      )}
+    </main>
   );
 }
 
 export default App;
+
